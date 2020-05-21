936d8b1df81777fbf3e88e77366aaf85526c4d95
Thomas Martinez <tmartinez114@gmail.com>
2020-05-21 02:25:12 -0400

    added String url to DB

--- mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
+++ mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
@@ -3,8 +3,7 @@ package net.codejava;
 public class JavaMySQLTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String url = "jdbc:mysql://localhost:3306/db_name";
 	}
 
 }
