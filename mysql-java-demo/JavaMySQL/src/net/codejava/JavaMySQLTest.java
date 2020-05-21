f1f506a053c00ddc9ecfc6463b55d31da4d66da6
Thomas Martinez <tmartinez114@gmail.com>
2020-05-21 02:51:47 -0400

    add new row of data to DB

--- mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
+++ mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
@@ -2,6 +2,7 @@ package net.codejava;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class JavaMySQLTest {
@@ -13,8 +14,22 @@ public class JavaMySQLTest {
 		
 		try {
 			Connection connection = DriverManager.getConnection(url, username, password);
-			
 			System.out.println("Connected to the database!");
+			
+			// Add new row of data to DB
+			String sql = "INSERT INTO customer (firstname, lastname) VALUES (?, ?)";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setString(1,  "Thomas"); // this will be placed in 1st ?
+			statement.setString(2,  "Martinez"); // this will be place in 2nd ?
+			
+			int rows = statement.executeUpdate();
+			if (rows > 0) {
+				System.out.println("A row has been inserted.");
+			}
+			
+			statement.close();
+			connection.close();
+			
 		} catch (SQLException e) {
 			System.out.println("Oops, error!");
 			e.printStackTrace();
