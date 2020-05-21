00f08524de738225854634514989c871abd478fc
Thomas Martinez <tmartinez114@gmail.com>
2020-05-21 02:34:47 -0400

    added TryCatch for connecting to DB

--- mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
+++ mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
@@ -2,15 +2,23 @@ package net.codejava;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class JavaMySQLTest {
 
 	public static void main(String[] args) {
 		String url = "jdbc:mysql://localhost:3306/db_name";
 		String username = "root";
-		String password = 'Password123#';
+		String password = "Password123#";
 		
-		Connection connection = DriverManager.getConnection(url, username, password);
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+			
+			System.out.println("Connected to the database!");
+		} catch (SQLException e) {
+			System.out.println("Oops, error!");
+			e.printStackTrace();
+		}
 	}
 
 }
