f9251364e7791d6ef0ce00bd1cf0d93c67108b73
Thomas Martinez <tmartinez114@gmail.com>
2020-05-21 02:28:15 -0400

    added username and password for db

--- mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
+++ mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
@@ -4,6 +4,8 @@ public class JavaMySQLTest {
 
 	public static void main(String[] args) {
 		String url = "jdbc:mysql://localhost:3306/db_name";
+		String username = "root";
+		String password = 'Password123#';
 	}
 
 }
