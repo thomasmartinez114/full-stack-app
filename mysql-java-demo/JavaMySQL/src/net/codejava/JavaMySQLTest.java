9782836ff96c0cd68846af52505e43f7f1a9b6b5
Thomas Martinez <tmartinez114@gmail.com>
2020-05-21 02:31:16 -0400

    connected to the DB

--- mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
+++ mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
@@ -1,11 +1,16 @@
 package net.codejava;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class JavaMySQLTest {
 
 	public static void main(String[] args) {
 		String url = "jdbc:mysql://localhost:3306/db_name";
 		String username = "root";
 		String password = 'Password123#';
+		
+		Connection connection = DriverManager.getConnection(url, username, password);
 	}
 
 }
