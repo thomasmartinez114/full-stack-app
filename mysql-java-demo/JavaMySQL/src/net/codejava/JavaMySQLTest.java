ddad904e4f2e463984673a7b725c0fc7e58b7d0d
Thomas Martinez <tmartinez114@gmail.com>
2020-05-21 22:09:52 -0400

    pull the data from DB into Eclipse

--- mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
+++ mysql-java-demo/JavaMySQL/src/net/codejava/JavaMySQLTest.java
@@ -3,7 +3,9 @@ package net.codejava;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JavaMySQLTest {
 
@@ -16,18 +18,34 @@ public class JavaMySQLTest {
 			Connection connection = DriverManager.getConnection(url, username, password);
 			System.out.println("Connected to the database!");
 			
-			// Add new row of data to DB
+/*			// Add new row of data to DB
 			String sql = "INSERT INTO customer (firstname, lastname) VALUES (?, ?)";
 			PreparedStatement statement = connection.prepareStatement(sql);
-			statement.setString(1,  "Christopher"); // this will be placed in 1st ?
-			statement.setString(2,  "Wallace"); // this will be place in 2nd ?
+			statement.setString(1,  "Nasir"); // this will be placed in 1st ?
+			statement.setString(2,  "Jones"); // this will be place in 2nd ?
 			
 			int rows = statement.executeUpdate();
 			if (rows > 0) {
 				System.out.println("A row has been inserted.");
+				}
+				
+				statement.close();
+*/
+			
+			// Selecting the data
+			String sql = "SELECT * FROM customer";
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			
+			int count = 0;
+			
+			while (result.next()) {
+				String firstName = result.getString("firstname");
+				String lastName = result.getString("lastname");
+				count++;
+				System.out.println("Customer " + count + ": " + firstName + " " + lastName);
 			}
 			
-			statement.close();
 			connection.close();
 			
 		} catch (SQLException e) {
