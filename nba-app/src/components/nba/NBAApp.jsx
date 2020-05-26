340147c8264fe9dd2f49be26b9b8ca6d4394dce9
Thomas Martinez <tmartinez114@gmail.com>
2020-05-26 02:01:41 -0400

    created LoginComponent and rendered to the NBAApp

--- nba-app/src/components/nba/NBAApp.jsx
+++ nba-app/src/components/nba/NBAApp.jsx
@@ -4,7 +4,19 @@ class NBAApp extends Component {
     render() {
         return (
             <div className="NBAApp">
-                My NBA Application
+                <LoginComponent />
+            </div>
+        )
+    }
+}
+
+class LoginComponent extends Component {
+    render() {
+        return (
+            <div>
+                User Name: <input type="text" name="username" />
+                Password: <input type="password" name="password" />
+                <button>Login</button>
             </div>
         )
     }
