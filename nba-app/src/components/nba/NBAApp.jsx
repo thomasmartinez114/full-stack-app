d4af15be75a5d5f10b60dd9a325f61e6997fbc16
Thomas Martinez <tmartinez114@gmail.com>
2020-05-26 02:15:12 -0400

    updated username field - controlled field updated by state

--- nba-app/src/components/nba/NBAApp.jsx
+++ nba-app/src/components/nba/NBAApp.jsx
@@ -4,6 +4,7 @@ class NBAApp extends Component {
     render() {
         return (
             <div className="NBAApp">
+                NBA API
                 <LoginComponent />
             </div>
         )
@@ -11,11 +12,26 @@ class NBAApp extends Component {
 }
 
 class LoginComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: 'tmartinez',
+            password: ''
+        }
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    }
+
+    handleUsernameChange(event) {
+        console.log(event.target.value);
+        this.setState({ username: event.target.value }
+        )
+    }
+
     render() {
         return (
             <div>
-                User Name: <input type="text" name="username" />
-                Password: <input type="password" name="password" />
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange} />
+                Password: <input type="password" name="password" vale={this.state.password} />
                 <button>Login</button>
             </div>
         )
