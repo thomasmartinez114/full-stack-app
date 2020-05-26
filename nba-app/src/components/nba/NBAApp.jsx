a91b53c81d023be2177014f86539b40d1a28c5f0
Thomas Martinez <tmartinez114@gmail.com>
2020-05-26 02:26:26 -0400

    refactored the Change Event to handle all changes - so not hard coding for each event change!

--- nba-app/src/components/nba/NBAApp.jsx
+++ nba-app/src/components/nba/NBAApp.jsx
@@ -18,26 +18,38 @@ class LoginComponent extends Component {
             username: 'tmartinez',
             password: ''
         }
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        // this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        // this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleUsernameChange(event) {
-        console.log(event.target.value);
-        this.setState({ username: event.target.value }
+
+    handleChange(event) {
+        console.log(this.state);
+        this.setState(
+            {
+                [event.target.name]
+                    : event.target.value
+            }
         )
     }
 
-    handlePasswordChange(event) {
-        console.log(event.target.value);
-        this.setState({ password: event.target.value })
-    }
+    // handleUsernameChange(event) {
+    //     console.log(event.target.value);
+    //     this.setState({ username: event.target.value }
+    //     )
+    // }
+
+    // handlePasswordChange(event) {
+    //     console.log(event.target.value);
+    //     this.setState({ password: event.target.value })
+    // }
 
     render() {
         return (
             <div>
-                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleUsernameChange} />
-                Password: <input type="password" name="password" vale={this.state.password} onChange={this.handlePasswordChange} />
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                Password: <input type="password" name="password" vale={this.state.password} onChange={this.handleChange} />
                 <button>Login</button>
             </div>
         )
