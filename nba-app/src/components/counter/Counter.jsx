172582a1d40782cce234cea666a0c3a55204601e
Thomas Martinez <tmartinez114@gmail.com>
2020-05-26 01:43:38 -0400

    created state - must use super() to use in the method - to call the counter valuable need to use {this.state.counter} in the <span>

--- nba-app/src/components/counter/Counter.jsx
+++ nba-app/src/components/counter/Counter.jsx
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import './Counter.css';
 
 class Counter extends Component {
+
+    //Define the initial state in a constructor
+    //state => counter 0
+    constructor() {
+        super(); //need to call super
+        this.state = {
+            counter: 0
+        }
+    }
+
     render() {
         return (
             <div className="counter">
                 <button onClick={this.increment}>+1</button>
-                <span className="count">0</span>
+                <span className="count">{this.state.counter}</span>
             </div>
         )
     }
 
-    increment() {
-        console.log('increment');
+    increment() { //Update state -> counter++
+        // console.log('increment');
+        this.state.counter++;
     }
 }
 
