c18e2a4580421d780d3575cd1ddf43f881c89601
Thomas Martinez <tmartinez114@gmail.com>
2020-05-26 01:31:20 -0400

    converted to Class Component from function

--- nba-app/src/components/counter/Counter.jsx
+++ nba-app/src/components/counter/Counter.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Counter.css';
 
-function Counter() {
-    return (
-        <div className="counter">
-            <button onClick={increment}>+1</button>
-            <span className="count">0</span>
-        </div>
-    )
+class Counter extends Component {
+    render() {
+        return (
+            <div className="counter">
+                <button onClick={increment}>+1</button>
+                <span className="count">0</span>
+            </div>
+        )
+    }
 }
 
 function increment() {
