105969fd8c0282623be68b9044dbaec5c37a6677
Thomas Martinez <tmartinez114@gmail.com>
2020-06-01 22:26:07 -0400

    header & footer component

--- nba-app/src/components/nba/NBAApp.jsx
+++ nba-app/src/components/nba/NBAApp.jsx
@@ -6,6 +6,7 @@ class NBAApp extends Component {
         return (
             <div className="NBAApp">
                 <Router>
+                    <HeaderComponent />
                     <Switch>
                         <Route path="/" exact component={LoginComponent} />
                         <Route path="/login" component={LoginComponent} />
@@ -13,10 +14,30 @@ class NBAApp extends Component {
                         <Route path="/stats" component={StatsComponent} />
                         <Route component={ErrorComponent} />
                     </Switch>
+                    <FooterComponent />
                 </Router>
 
                 {/* <LoginComponent />
                 <WelcomeComponent /> */}
+            </div>
+        )
+    }
+}
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div>
+                Header <hr />
+            </div>
+        )
+    }
+}
+class FooterComponent extends Component {
+    render() {
+        return (
+            <div>
+                <hr /> Footer
             </div>
         )
     }
