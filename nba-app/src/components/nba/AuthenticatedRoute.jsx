3325065c21b2a822de96e502c779e64503105fd0
Thomas Martinez <tmartinez114@gmail.com>
2020-06-04 19:59:21 -0400

    Secure Components with Auth. Route - implemented else/if statement and used Spread Operator in AuthenticateRoute.jsx

--- nba-app/src/components/nba/AuthenticatedRoute.jsx
+++ nba-app/src/components/nba/AuthenticatedRoute.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthenticationService from './AuthenticationService.js';
 
 class AuthenticatedRoute extends Component {
     render() {
-
+        if (AuthenticationService.isUserLoggedIn()) {
+            <Route {...this.props} /> // spread operator
+        } else {
+            <Redirect to="/login" />
+        }
     }
 }
