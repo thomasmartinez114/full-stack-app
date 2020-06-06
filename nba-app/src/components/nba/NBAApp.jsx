bc3b2e817bc276b0dcab504dbd2a13063e2a1df5
Thomas Martinez <tmartinez114@gmail.com>
2020-06-05 23:57:14 -0400

    Refactor Components - StatsComponent

--- nba-app/src/components/nba/NBAApp.jsx
+++ nba-app/src/components/nba/NBAApp.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import AuthenticationService from './AuthenticationService.js';
 import AuthenticatedRoute from './AuthenticatedRoute.jsx';
 import LoginComponent from './LoginComponent.jsx';
+import StatsComponent from './StatsComponent.jsx';
 
 class NBAApp extends Component {
     render() {
@@ -70,55 +71,6 @@ class LogoutComponent extends Component {
                     Thank you for using our Application.
                 </div>
             </>
-        )
-    }
-}
-
-class StatsComponent extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            stats:
-                [
-                    { id: 1, team: 'LAL', player: 'LeBron James', ppg: 27.0, allStar: true },
-                    { id: 2, team: 'GSW', player: 'Steph Curry', ppg: 25.0, allStar: true },
-                    { id: 3, team: 'BOS', player: 'Jayson Tatum', ppg: 18.0, allStar: true }
-                ]
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <h4>More NBA Statistics coming soon. . . .</h4>
-                <div className="container">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Rank</th>
-                                <th>Team</th>
-                                <th>Player</th>
-                                <th>PPG</th>
-                                <th>All-Star Selection</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.stats.map(
-                                    stat =>
-                                        <tr key={stat.id}>
-                                            <td>{stat.id}</td>
-                                            <td>{stat.team}</td>
-                                            <td>{stat.player}</td>
-                                            <td>{stat.ppg}</td>
-                                            <td>{stat.allStar.toString()}</td>
-                                        </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
-                </div>
-            </div>
         )
     }
 }
