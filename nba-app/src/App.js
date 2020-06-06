56e6a53ddc5656c5dc26ca77b2cde73728cbc039
Thomas Martinez <tmartinez114@gmail.com>
2020-06-06 00:32:31 -0400

    refactor - removed unused compoents from examples and moved the NBAApp compoenents to the components root folder

--- nba-app/src/App.js
+++ nba-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import NBAApp from './components/nba/NBAApp';
+import NBAApp from './components/NBAApp';
 import './App.css';
 import './bootstrap.css';
 
@@ -7,25 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Counter/> */}
         <NBAApp />
       </div>
     );
   }
 }
-
-// class LearningComponents extends Component {
-//   render() {
-//     return (
-//       <div className="LearningComponents">
-//         My Hello World
-//         <FirstComponent />
-//         <SecondComponent />
-//         <ThirdComponent />
-//       </div>
-//     );
-//   }
-// }
-
 
 export default App;
