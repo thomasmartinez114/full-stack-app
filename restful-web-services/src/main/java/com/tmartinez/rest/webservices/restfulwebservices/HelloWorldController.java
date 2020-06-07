09fe4eba74912c6a1e0ea8b8fead65a7fb1b2a58
Thomas Martinez <tmartinez114@gmail.com>
2020-06-07 01:30:38 -0400

    Create a Spring Boot Hello World Service - Refactored mapping by using @GetMapping

--- restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,7 +1,7 @@
 package com.tmartinez.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
@@ -11,7 +11,7 @@ public class HelloWorldController {
 	//GET
 	//URI - /hello-world
 	//method - "Hello world"
-	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
