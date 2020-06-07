8a2d059292d7668470c0216fce2d32c9ce91d3c7
Thomas Martinez <tmartinez114@gmail.com>
2020-06-07 01:25:08 -0400

    Create a Spring Boot Hello World Service - Set Controller

--- restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,9 +1,16 @@
 package com.tmartinez.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
 public class HelloWorldController {
 	
 	//GET
 	//URI - /hello-world
 	//method - "Hello world"
+	public String helloWorld() {
+		return "Hello World";
+	}
 
 }
