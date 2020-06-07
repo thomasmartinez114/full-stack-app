7776bf3966415a66d450a5c6e9df7647cc53c133
Thomas Martinez <tmartinez114@gmail.com>
2020-06-07 01:26:24 -0400

    Create a Spring Boot Hello World Service - Set Mapping to set path link

--- restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.tmartinez.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
@@ -9,6 +11,7 @@ public class HelloWorldController {
 	//GET
 	//URI - /hello-world
 	//method - "Hello world"
+	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
