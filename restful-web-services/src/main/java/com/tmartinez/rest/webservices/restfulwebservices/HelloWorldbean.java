daaffc3d68d0005be27706014866acfa12f6e489
Thomas Martinez <tmartinez114@gmail.com>
2020-06-09 00:02:22 -0400

    created a getter to clear the error message when navigating to localhost:8080/hello-world-bean

--- restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldbean.java
+++ restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldbean.java
@@ -7,6 +7,13 @@ public class HelloWorldBean {
 	public HelloWorldBean(String message) {
 		this.message = message;
 	}
+	
+	
+	public String getMessage() {
+		return message;
+	}
+
+
 
 	public void setMessage(String message) {
 		this.message = message;
