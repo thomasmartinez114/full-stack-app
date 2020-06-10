494f2e542f5b11ca2fd6fd6a058694a4e57c692e
Thomas Martinez <tmartinez114@gmail.com>
2020-06-09 23:20:10 -0400

    05-30 Path Variables on Hello World

--- restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.tmartinez.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
@@ -20,6 +21,12 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	//hello-world/path-variable/tmartinez
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 
 }
