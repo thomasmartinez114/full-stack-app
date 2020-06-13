c845aba85e7c609444cc575786b613c34af6c429
Thomas Martinez <tmartinez114@gmail.com>
2020-06-12 23:53:59 -0400

    updated app name

--- nba-app/src/components/HeaderComponent.jsx
+++ nba-app/src/components/HeaderComponent.jsx
@@ -11,7 +11,7 @@ class HeaderComponent extends Component {
         return (
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-info">
-                    <div><a href="https://stats.nba.com/" className="navbar-brand">nbastats</a></div>
+                    <div><a href="https://stats.nba.com/" className="navbar-brand">NBA App</a></div>
                     <ul className="navbar-nav">
                         {isUserLoggedIn && <li><Link className="nav-link" to="/welcome/nbastats">Home</Link></li>}
                         {isUserLoggedIn && <li><Link className="nav-link" to="/stats">Stats</Link></li>}
