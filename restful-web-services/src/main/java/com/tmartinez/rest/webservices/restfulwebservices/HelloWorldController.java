cad28948142a2ddde44a96d7c8c8079c42856219
Thomas Martinez <tmartinez114@gmail.com>
2020-06-13 14:05:35 -0400

    06-35 - @CrossOrigin to get response from backend to front

--- restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,12 +1,14 @@
 package com.tmartinez.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
 	
 	//GET
