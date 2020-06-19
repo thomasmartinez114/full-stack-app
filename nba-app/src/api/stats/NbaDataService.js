1895dfd43863353783f8e7ce0a672435744f47a1
Thomas Martinez <tmartinez114@gmail.com>
2020-06-19 18:35:09 -0400

    code displaying on front end react

--- nba-app/src/api/stats/NbaDataService.js
+++ nba-app/src/api/stats/NbaDataService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 class NbaDataService {
     retrieveAllNba() {
-        return axios.get(`http://localhost:8080/players/nbas`);
+        return axios.get(`http://localhost:8080/nba/stats`);
     }
 }
 
