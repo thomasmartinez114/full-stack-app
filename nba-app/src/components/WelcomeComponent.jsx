eb6b9b11fe284b0d2f74f7a780dca418f72ae9d5
Thomas Martinez <tmartinez114@gmail.com>
2020-06-22 00:08:27 -0400

    resolved bug of /nba/stats URL not functinong properly bringing to Component

--- nba-app/src/components/WelcomeComponent.jsx
+++ nba-app/src/components/WelcomeComponent.jsx
@@ -19,7 +19,7 @@ class WelcomeComponent extends Component {
                 <h1>Welcome!</h1>
                 <div className="container">
                     Welcome <b>{this.props.match.params.name}</b> to the NBA API.<br></br>
-                    You can view NBA stats <Link to="/stats">here</Link>
+                    You can view NBA stats <Link to="/nba/stats">here</Link>
                 </div>
                 <div className="container">
                     Click here to get a customized welcome message.
