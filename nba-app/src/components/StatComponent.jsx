2f39f930c8c8bd46aa28bb18296440de01dfc0f1
Thomas Martinez <tmartinez114@gmail.com>
2020-06-26 23:18:53 -0400

    Designing Stat Form - set constructor

--- nba-app/src/components/StatComponent.jsx
+++ nba-app/src/components/StatComponent.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class StatComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: 1,
+            firstName: 'Larry',
+            lastName: 'Bird',
+            ppg: 30
+        }
+    }
+
     render() {
         return <div>Stat Component for id - {this.props.match.params.id}</div>
     }
