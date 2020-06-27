da18dd16b8beb49e666b9afec240bc2c3ec32118
Thomas Martinez <tmartinez114@gmail.com>
2020-06-27 00:13:27 -0400

    created Form using Formik

--- nba-app/src/components/StatComponent.jsx
+++ nba-app/src/components/StatComponent.jsx
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
+import { Formik, Form, Field } from 'formik';
 
 class StatComponent extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            id: 1,
+            id: this.props.match.params.id,
             firstName: 'Larry',
             lastName: 'Bird',
+            position: 'SF',
+            team: 'Boston',
             ppg: 30
         }
     }
 
     render() {
-        return <div>Stat Component for id - {this.props.match.params.id}</div>
+        return (<div>
+            <h1>Stat</h1>
+            <div className="container">
+                <Formik>
+                    {
+                        (props) => (
+                            <Form>
+                                <fieldset className="form-group">
+                                    <label>First Name</label>
+                                    <Field className="form-control" type="text" name="firstName" />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>lastName Name</label>
+                                    <Field className="form-control" type="text" name="lastName" />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Position</label>
+                                    <Field className="form-control" type="text" name="position" />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Team</label>
+                                    <Field className="form-control" type="text" name="Team" />
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>PPG</label>
+                                    <Field className="form-control" type="text" name="ppg" />
+                                </fieldset>
+                                <button className="btn btn-success" type="submit">Save</button>
+                            </Form>
+                        )
+                    }
+                </Formik>
+
+            </div>
+        </div>
+        )
     }
 }
 
