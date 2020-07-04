2854f48b642d6d914f6a9f08953d028e6139b983
Thomas Martinez <tmartinez114@gmail.com>
2020-07-03 22:46:22 -0400

    Edit/Update a stat using @PutMapping

--- restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/nba/NbaResource.java
+++ restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/nba/NbaResource.java
@@ -3,11 +3,14 @@ package com.tmartinez.rest.webservices.restfulwebservices.nba;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 //import com.tmartinez.rest.webservices.restfulwebservices.nba.Nba;
@@ -45,5 +48,21 @@ public class NbaResource {
 		
 		return ResponseEntity.notFound().build();
 	}
+	
+	// Edit/Update a Stat
+	// PUT /nba/stats/{id}
+	@PutMapping("nba/stats/{id}")
+	public ResponseEntity<Nba> updateNba(
+			@PathVariable long id,
+			@RequestBody Nba nba) {
+		
+		Nba nbaUpdated = nbaService.save(nba);
+		
+		return new ResponseEntity<Nba>(nba, HttpStatus.OK);
+		
+	}
+	
+	//Create a new Stat
+	//POST /nba/stats/
 	
 }
