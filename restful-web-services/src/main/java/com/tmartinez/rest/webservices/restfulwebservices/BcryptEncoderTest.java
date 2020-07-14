96a3389a5d9e8db02272565a9e8484437909f8af
Thomas Martinez <tmartinez114@gmail.com>
2020-07-14 00:11:48 -0400

    Printed out the encodedString of password from for loop iteration

--- restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/BcryptEncoderTest.java
+++ restful-web-services/src/main/java/com/tmartinez/rest/webservices/restfulwebservices/BcryptEncoderTest.java
@@ -5,9 +5,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class BcryptEncoderTest {
 
 	public static void main(String[] args) {
-		BCryptPasswordEncoder encoder = new BcryptPasswordEncoder();
-		String encodedString = encoder.encode("password1234@@");
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		
+		for(int i=1; i<=10; i++) {
+		String encodedString = encoder.encode("password@!23#!");
+		System.out.println(encodedString);
+		}
 		// TODO Auto-generated method stub
 
 	}
