26f6f9d287aadaeed6d3ddd4eabe78073bf597da
Martinez <thomas.martinez@igt.com>
2020-08-14 15:26:02 -0400

    removed the Util

--- restful-web-services/src/main/java/com/martinez/rest/webservices/restfulwebservices/mysql/utilities/Util.java
+++
@@ -1,7 +0,0 @@
-package com.martinez.rest.webservices.restfulwebservices.mysql.utilities;
-
-public abstract class Util {
-    public static String jdbcUrl="jdbc:mysql://localhost:3306/nba_stats";
-    public static String user="root"; // nbaadmin
-    public static String pass="databas3"; // NBAadmin#
-}
